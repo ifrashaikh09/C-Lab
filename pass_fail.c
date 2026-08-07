289eb717f84c3240e64f28fa42af248892c883c8
ifrashaikh09 <ifrashahid992007@gmail.com>
2026-08-07 14:12:42 +0530

    Add pass/fail program using ternary operator

---
+++ pass_fail.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  float per;
+  printf("\n enter your percentage:");
+  scanf("%f",&per);
+
+  (per>=35)? printf("\n pass"): printf("\n fail");
+
+  return 0;
+}
