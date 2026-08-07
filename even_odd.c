55af3b16292735327cb839119fee38976805f721
ifrashaikh09 <ifrashahid992007@gmail.com>
2026-08-07 18:43:57 +0530

    Add even odd program using if-else

---
+++ even_odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("\n enter any number:");
+    scanf("%d",&n);
+
+    if(n%2==0)
+     printf("\n Even");
+    else
+     printf("\n Odd");
+
+   return 0;
+}
